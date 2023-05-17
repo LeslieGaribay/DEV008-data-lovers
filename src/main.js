720a9ee6ddd496cf6b80afbf03296af829c48390
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-05-17 08:29:11 -0600

    Diseño básico de estructura en HTML y CSS

--- src/main.js
+++ src/main.js
@@ -4,3 +4,7 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+document.getElementById("searchbutton").addEventListener("click");
+    alert("Hola Mundo!")
+
