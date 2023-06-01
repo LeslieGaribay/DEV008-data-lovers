36f5b66ef375ce834b86e0b12f7417e794af2fba
Erika Nagaki <enagaki.22@gmail.com>
2023-06-01 16:44:00 -0500

    fix formating

--- src/data.js
+++ src/data.js
@@ -13,11 +13,11 @@ export function SortByMovies(data, sortByProperty, sortOrder) {
 
   filteredMovies = data.films.sort((a, b) => {
     if (a[sortByProperty] > b[sortByProperty]) {
-        return 1 * sortOrder;
+      return 1 * sortOrder;
     } else if (b[sortByProperty] > a[sortByProperty]) {
-        return -1 * sortOrder;
+      return -1 * sortOrder;
     } else {
-        return 0;
+      return 0;
     }
   });
 
--- src/main.js
+++ src/main.js
@@ -12,44 +12,44 @@ import { SortByMovies, FilterData } from './data.js'
 
 // Crear un template para cada película en el arreglo del Ghibli.js:
 
-let movieTemplate = document.getElementById("movie-template"); // Traemos el template que vamos a estar clonando
-let movieSection = document.querySelector(".index-movies-library"); // Traemos donde vamos a mostrar los clones del template
+const movieTemplate = document.getElementById("movie-template"); // Traemos el template que vamos a estar clonando
+const movieSection = document.querySelector(".index-movies-library"); // Traemos donde vamos a mostrar los clones del template
 
 for (let movieIndex = 0; movieIndex < data.films.length; movieIndex++) { // Vamos a iterar por todas las películas que están en el data
-    let movie = data.films[movieIndex]; // Declaramos la película que corresponde a la iteración actual
+  const movie = data.films[movieIndex]; // Declaramos la película que corresponde a la iteración actual
 
-    let templateClone = movieTemplate.content.cloneNode(true); // Declaramos el clon, y con el content.cloneNode y el true le decimos que clone el elemento con todos los hijos
-    let moviePoster = templateClone.querySelector(".movie-poster"); // Es como el document.getElementDyId pero con selector en lugar de ID, y sólo busca dentro del templateClone, en lugar de todo el documento
-    let movieTitle = templateClone.querySelector(".movie-title");
-    let movieYear = templateClone.querySelector(".movie-year");
+  const templateClone = movieTemplate.content.cloneNode(true); // Declaramos el clon, y con el content.cloneNode y el true le decimos que clone el elemento con todos los hijos
+  const moviePoster = templateClone.querySelector(".movie-poster"); // Es como el document.getElementDyId pero con selector en lugar de ID, y sólo busca dentro del templateClone, en lugar de todo el documento
+  const movieTitle = templateClone.querySelector(".movie-title");
+  const movieYear = templateClone.querySelector(".movie-year");
 
-    movieTitle.innerText = movie.title; // .title: así se llama ese dato en ghibli.js, aquí se cambian los valores del template por los de la película de la iteración actual
-    movieYear.innerText = "(" + movie.release_date + ")"; // .release_date: así se llama ese dato en ghibli.js
-    moviePoster.src = movie.poster; // .poster: así se llama ese dato en ghibli.js
+  movieTitle.innerText = movie.title; // .title: así se llama ese dato en ghibli.js, aquí se cambian los valores del template por los de la película de la iteración actual
+  movieYear.innerText = "(" + movie.release_date + ")"; // .release_date: así se llama ese dato en ghibli.js
+  moviePoster.src = movie.poster; // .poster: así se llama ese dato en ghibli.js
 
-    movieSection.appendChild(templateClone); // Aquí estamos poniendo el clon en el movieSection
+  movieSection.appendChild(templateClone); // Aquí estamos poniendo el clon en el movieSection
 
 }
 
 function addFilmToLibrary(film) {
-    let templateClone = movieTemplate.content.cloneNode(true);
-    let moviePoster = templateClone.querySelector(".movie-poster");
-    let movieTitle = templateClone.querySelector(".movie-title");
-    let movieYear = templateClone.querySelector(".movie-year");
+  const templateClone = movieTemplate.content.cloneNode(true);
+  const moviePoster = templateClone.querySelector(".movie-poster");
+  const movieTitle = templateClone.querySelector(".movie-title");
+  const movieYear = templateClone.querySelector(".movie-year");
 
-    movieTitle.innerText = film.title;
-    movieYear.innerText = "(" + film.release_date + ")";
-    moviePoster.src = film.poster;
+  movieTitle.innerText = film.title;
+  movieYear.innerText = "(" + film.release_date + ")";
+  moviePoster.src = film.poster;
 
-    movieSection.appendChild(templateClone);
+  movieSection.appendChild(templateClone);
 }
 
 
 function addFilmsToLibrary(films) {// Para el boton de busqueda
-    for (let movieIndex = 0; movieIndex < films.length; movieIndex++) {
-        const filteredMovie = films[movieIndex];
-        addFilmToLibrary(filteredMovie);
-    }
+  for (let movieIndex = 0; movieIndex < films.length; movieIndex++) {
+    const filteredMovie = films[movieIndex];
+    addFilmToLibrary(filteredMovie);
+  }
 }
 
 
@@ -57,15 +57,15 @@ const filterButtons = document.querySelectorAll(".filter-list .sub-filter-list-b
 filterButtons.forEach(filterButton => filterButton.addEventListener("click", FilterMoviesWrapper))
 
 function FilterMoviesWrapper (event) {
-    const button = event.target
-    const filmProperty = button.dataset.property // director
-    const filmPropertyValue = button.dataset.value // Hayao Miyazaki
+  const button = event.target
+  const filmProperty = button.dataset.property // director
+  const filmPropertyValue = button.dataset.value // Hayao Miyazaki
 
-    let filteredFilms = FilterData(data, filmProperty, filmPropertyValue);
+  const filteredFilms = FilterData(data, filmProperty, filmPropertyValue);
 
-    movieSection.innerHTML = "";
-    addFilmsToLibrary(filteredFilms)
-    }
+  movieSection.innerHTML = "";
+  addFilmsToLibrary(filteredFilms)
+}
 
 
 
@@ -75,19 +75,19 @@ const searchInput = document.getElementById("searchBar");
 searchInput.addEventListener("input", SearchMovies);
 
 
-function SearchMovies(event) {
+function SearchMovies() {
 
-    movieSection.innerHTML = "";
+  movieSection.innerHTML = "";
 
-    // Agregar código para traer el input text, y sacar el texto de búsqueda
-    const searchBarValue = document.getElementById("searchBar").value;
+  // Agregar código para traer el input text, y sacar el texto de búsqueda
+  const searchBarValue = document.getElementById("searchBar").value;
 
-    let filteredMovies = null;
+  let filteredMovies = null;
 
-    // Agregar código para filtrar por título (solo por mientras, luego por más propiedades)
-    filteredMovies = data.films.filter(film => film["title"].toLowerCase().includes(searchBarValue.toLowerCase()));
+  // Agregar código para filtrar por título (solo por mientras, luego por más propiedades)
+  filteredMovies = data.films.filter(film => film["title"].toLowerCase().includes(searchBarValue.toLowerCase()));
 
-    addFilmsToLibrary(filteredMovies)
+  addFilmsToLibrary(filteredMovies)
 }
 
 
@@ -96,22 +96,22 @@ const sortByButtons = document.querySelectorAll(".sort-by-option")
 sortByButtons.forEach(sortByButton => sortByButton.addEventListener("click", SortByMoviesWrapper))
 
 function SortByMoviesWrapper (event) {
-    const button = event.target
-    const sortByProperty = button.dataset.property
+  const button = event.target
+  const sortByProperty = button.dataset.property
     
-    let sortOrder = 1;
-    switch (button.dataset.order) {
-        case "asc":
-            sortOrder = 1; // Ascendente
-            break;
-        case "desc":
-            sortOrder = -1; // Descendente
-            break;
-    }
+  let sortOrder = 1;
+  switch (button.dataset.order) {
+  case "asc":
+    sortOrder = 1; // Ascendente
+    break;
+  case "desc":
+    sortOrder = -1; // Descendente
+    break;
+  }
 
-    let sortedMovies = SortByMovies(data, sortByProperty, sortOrder)
+  const sortedMovies = SortByMovies(data, sortByProperty, sortOrder)
     
     
-    movieSection.innerHTML = "";
-    addFilmsToLibrary(sortedMovies)
+  movieSection.innerHTML = "";
+  addFilmsToLibrary(sortedMovies)
 }
