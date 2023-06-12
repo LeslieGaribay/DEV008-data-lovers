4268b17ff91c2b5e10c79e75df0f03504da37c91
LeslieGaribay <131633753+LeslieGaribay@users.noreply.github.com>
2023-06-12 12:08:02 -0600

    Updates

---
+++ data.js
@@ -0,0 +1,67 @@
+
+export function FilterData(films, filmProperty, filmPropertyValue) {
+  if (!Array.isArray(films)) {
+    throw new TypeError('films is not an array')
+  }
+
+  if (!filmProperty) {
+    throw new TypeError('filmProperty can not be undefined')
+  }
+
+  if (!filmPropertyValue) {
+    throw new TypeError('filmPropertyValue can not be undefined')
+  }
+ 
+  let filteredMovies = [];
+  filteredMovies = films.filter(film => film[filmProperty] === filmPropertyValue);
+  return filteredMovies;
+}
+
+
+//sortData(data, sortBy, sortOrder=[1,-1])
+export function SortByMovies(films, sortByProperty, sortOrder) {
+  if (!Array.isArray(films)) {
+    throw new TypeError('films is not an array')
+  }
+  
+  if (!sortByProperty) {
+    throw new TypeError('sortByProperty can not be undefined')
+  }
+
+  let sortedMovies = [];
+  sortedMovies = films.sort((a, b) => {
+    if (a[sortByProperty] > b[sortByProperty]) {
+      return 1 * sortOrder;
+    } else if (b[sortByProperty] > a[sortByProperty]) {
+      return -1 * sortOrder;
+    } else {
+      return 0;
+    }
+  });
+
+  return sortedMovies;
+}
+
+let filteredAndSortedMovies = []; // guardar estado 
+export function FilterAndSortMovies(films, filmProperty, filmPropertyValue, sortByProperty, sortOrder){
+  if (!Array.isArray(films)) {
+    throw new TypeError('films is not an array')
+  }
+
+  if (filteredAndSortedMovies.length === 0){
+    filteredAndSortedMovies = films;
+  }
+
+  if (filmProperty && filmPropertyValue){
+    filteredAndSortedMovies = FilterData(films, filmProperty, filmPropertyValue); 
+  } 
+  
+  if (sortByProperty && sortOrder){
+    filteredAndSortedMovies = SortByMovies(filteredAndSortedMovies, sortByProperty, sortOrder);
+  }
+  return filteredAndSortedMovies;
+}
+
+export function resetFilteredAndSortedMovies() {
+  filteredAndSortedMovies = []
+}
---
+++ main.js
@@ -0,0 +1,138 @@
+// en este import data.js se traen las funciones filterdata / sortdata / computestats
+import data from './data/ghibli/ghibli.js'; // lo que nos trae la base de datos GHIBLI
+import { FilterAndSortMovies, FilterData } from './data.js'
+
+// let movieTitlesFromDataBase = data.films.map(film => film.title);
+// const movieLibrary = document.getElementById('movie-library');
+// let firstMovieTitle = movieTitlesFromDataBase[0];
+// movieLibrary.innerText = firstMovieTitle;
+
+// let director = data.films.map(film => film.director);
+
+// Crear un template para cada película en el arreglo del Ghibli.js:
+
+const movieTemplate = document.getElementById("movie-template"); // Traemos el template que vamos a estar clonando
+const movieSection = document.querySelector(".index-movies-library"); // Traemos donde vamos a mostrar los clones del template
+
+
+const movies = data.films;
+for (let movieIndex = 0; movieIndex < movies.length; movieIndex++) { // Vamos a iterar por todas las películas que están en el data
+  const movie = data.films[movieIndex]; // Declaramos la película que corresponde a la iteración actual
+
+  const templateClone = movieTemplate.content.cloneNode(true); // Declaramos el clon, y con el content.cloneNode y el true le decimos que clone el elemento con todos los hijos
+  const moviePoster = templateClone.querySelector(".movie-poster"); // Es como el document.getElementDyId pero con selector en lugar de ID, y sólo busca dentro del templateClone, en lugar de todo el documento
+  const movieTitle = templateClone.querySelector(".movie-title");
+  const movieYear = templateClone.querySelector(".movie-year");
+
+  movieTitle.innerText = movie.title; // .title: así se llama ese dato en ghibli.js, aquí se cambian los valores del template por los de la película de la iteración actual
+  movieYear.innerText = "(" + movie.release_date + ")"; // .release_date: así se llama ese dato en ghibli.js
+  moviePoster.src = movie.poster; // .poster: así se llama ese dato en ghibli.js
+
+  movieSection.appendChild(templateClone); // Aquí estamos poniendo el clon en el movieSection
+
+}
+
+function addFilmToLibrary(film) {
+  const templateClone = movieTemplate.content.cloneNode(true);
+  const moviePoster = templateClone.querySelector(".movie-poster");
+  const movieTitle = templateClone.querySelector(".movie-title");
+  const movieYear = templateClone.querySelector(".movie-year");
+
+  movieTitle.innerText = film.title;
+  movieYear.innerText = "(" + film.release_date + ")";
+  moviePoster.src = film.poster;
+
+  movieSection.appendChild(templateClone);
+}
+
+
+function addFilmsToLibrary(films) {// Para el boton de busqueda
+  for (let movieIndex = 0; movieIndex < films.length; movieIndex++) {
+    const filteredMovie = films[movieIndex];
+    addFilmToLibrary(filteredMovie);
+  }
+}
+
+
+const filterButtons = document.querySelectorAll(".filter-list .sub-filter-list-button")
+filterButtons.forEach(filterButton => {
+  const filtered = FilterData(data.films, filterButton.dataset.property, filterButton.dataset.value)
+  const counter = document.createElement('span')
+  counter.innerText = ` (${filtered.length}) `
+  filterButton.appendChild(counter)
+  filterButton.addEventListener("click", FilterMoviesWrapper)
+})
+
+
+function highlightButton (buttooooon) {
+  const filterButtons = document.querySelectorAll(".filter-list .sub-filter-list-button")
+  filterButtons.forEach(filterButton => filterButton.className = filterButton.className.replace(" jhgjhghfhghjgjhgjhg", ""))
+  buttooooon.className += " jhgjhghfhghjgjhgjhg";
+}
+
+function FilterMoviesWrapper (event) {
+  const button = event.target
+  highlightButton(button);
+  const filmProperty = button.dataset.property // director
+  const filmPropertyValue = button.dataset.value // Hayao Miyazaki
+
+  const filteredFilms = FilterAndSortMovies(data.films, filmProperty, filmPropertyValue, undefined, undefined);
+
+  movieSection.innerHTML = "";
+  addFilmsToLibrary(filteredFilms)
+}
+
+
+const searchInput = document.getElementById("searchBar");
+searchInput.addEventListener("input", SearchMovies);
+
+
+const MovieNotFoundTemplate = document.createElement("div")
+MovieNotFoundTemplate.classList.add("movie-not-found-container")
+const MovieNotFoundText = document.createElement("p")
+MovieNotFoundText.innerText = "No match found"
+MovieNotFoundText.classList.add("movie-not-found")
+MovieNotFoundTemplate.appendChild(MovieNotFoundText)
+
+function SearchMovies() {
+  movieSection.innerHTML = "";
+  // Agregar código para traer el input text, y sacar el texto de búsqueda
+  const searchBarValue = document.getElementById("searchBar").value;
+  // Agregar código para filtrar por título (solo por mientras, luego por más propiedades)
+  const filteredMovies = data.films.filter(film => film["title"].toLowerCase().includes(searchBarValue.toLowerCase()));
+  
+  if (filteredMovies.length === 0) {
+    movieSection.classList.remove("index-movies-library")
+    movieSection.appendChild(MovieNotFoundTemplate)
+  } else {
+    movieSection.classList.add("index-movies-library")
+    addFilmsToLibrary(filteredMovies)
+  }
+}
+
+
+
+const sortByButtons = document.querySelectorAll(".sort-by-option")
+sortByButtons.forEach(sortByButton => sortByButton.addEventListener("click", SortByMoviesWrapper))
+
+function SortByMoviesWrapper (event) {
+  const button = event.target
+  const sortByProperty = button.dataset.property
+    
+  let sortOrder = 1;
+  switch (button.dataset.order) {
+  case "asc":
+    sortOrder = 1; // Ascendente
+    break;
+  case "desc":
+    sortOrder = -1; // Descendente
+    break;
+  }
+
+  const sortedMovies = FilterAndSortMovies(data.films, undefined, undefined, sortByProperty, sortOrder)
+    
+    
+  movieSection.innerHTML = "";
+  addFilmsToLibrary(sortedMovies)
+}
+
